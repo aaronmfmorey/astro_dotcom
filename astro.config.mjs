0c626d258c9bdbe40281b34cc1b2bb1bf19ca1f9
Aaron Morey <aaronmorey@gmail.com>
2025-02-24 23:34:03 -0600

    Running through Astro tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,23 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	compressHTML: true,
+	// From https://docs.astro.build/en/reference/configuration-reference/#compresshtml
+	markdown: {
+		shikiConfig: {
+		  // Choose from Shiki's built-in themes (or add your own)
+		  // https://shiki.style/themes
+		  theme: 'everforest-dark',
+		  // Alternatively, provide multiple themes
+		  // See note below for using dual light/dark themes
+		//   themes: {
+		// 	light: 'github-light',
+		// 	dark: 'github-dark',
+		//   },
+		  // Disable the default colors
+		  // https://shiki.style/guide/dual-themes#without-default-color
+		  // (Added in v4.12.0)
+		  defaultColor: false,
+		}
+	}
 });
