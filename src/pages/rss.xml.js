869eb3be4db2bf5732d0ba9efe22bc8c9711485e
Aaron Morey <aaronmorey@gmail.com>
2025-03-04 07:50:21 -0600

    Add rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,13 +4,27 @@ import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
 	const posts = await getCollection('blog');
+	console.log("posts")
+	console.log(posts)
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
+		items: posts.sort(function (a, b) {
+			if(a.pubDate == b.pubDate) {
+				return 0;
+			} else if( a.pubDate < b.pubDate){
+				return -1;
+			}  else {
+				return 1;
+			}
+		})
+			.map(function (post) {
+			console.log(post.data);
+			return ({
+				...post.data,
+				link: `/blog/${post.id}/`,
+			})
+		}),
 	});
 }
